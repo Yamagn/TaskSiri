e0f537e18c7144eb9e4e76abd539fdcbd8479329
Yamagn <syankenpon@gmail.com>
2019-08-22 09:11:56 +0900

    Initial Commit

---
+++ TaskSiri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskSiri
+//
+//  Created by ymgn on 2019/08/22.
+//  Copyright © 2019 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
